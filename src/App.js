f2bbfb8c6f987a66958aea1c14feb01531b786ac
Djaa9 <jp@thepage.dk>
2020-07-18 01:56:17 +0200

    Choose Pirate faction

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       marginTop: theme.spacing(2)
     },
     button: {
-      marginTop: theme.spacing(2),
+      margin: theme.spacing(2),
       alignSelf: "left"
     }
   }));
@@ -30,22 +30,29 @@ function App() {
   const [gameMode, setGameMode] = useState("patrol");
   const [admiral, setAdmiral] = useState("Untested Admiral");
   const [availableAdmirals, setAvailableAdminirals] = useState([]);
+  const [availableInitiativeCards, setAvailableInitiativeCards] = useState([]);
 
   const handleGameModeChange = (event) => {
     setGameMode(event.target.value);
   };
 
   const handleFactionChange = (event) => {
-    var faction = factions.find(x => x.value === event.target.value);
+    var faction = factions.find(x => x.name === event.target.value);
     setFaction(faction);
-    setAdmiral("");
+    console.log(faction);
 
+    setAdmiral("");
     var admiralsToLoad = [];
     faction.admirals.forEach(admiral => {
       admiralsToLoad.push({ value: admiral, label: admiral });
     });
 
     setAvailableAdminirals(admiralsToLoad);
+
+    var availableInitiativeCards = genericInitiativeCards;
+
+    availableInitiativeCards.concat(faction.initiativeCards);
+
   };
 
   const handleAdmiralChange = (event) => {
@@ -59,7 +66,7 @@ function App() {
 
   const factions = [
     {
-      value: 'Dutch',
+      name: 'Dutch',
       admirals: [
         "Untested Admiral",
         "Experienced Admiral",
@@ -85,10 +92,32 @@ function App() {
         "Untested Inspiring Admiral",
         "Experienced Inspiring Admiral",
         "Seasoned Inspiring Admiral"
-      ]
+      ],
+      initiativeCards: [
+        { 
+        name: "Boat Assault",
+        initiativeValue: 1
+      },
+      { 
+        name: "Lured into the Shoals",
+        initiativeValue: 2
+      },
+      { 
+        name: "Expert Boarding",
+        initiativeValue: 3
+      },
+      { 
+        name: "Adaptive tactics",
+        initiativeValue: 4
+      },
+      { 
+        name: "Seize the Opportunity",
+        initiativeValue: 5
+      }
+    ]
     },
     {
-      value: 'English',
+      name: 'English',
       admirals: [
         "Untested Admiral",
         "Experienced Admiral",
@@ -114,10 +143,32 @@ function App() {
         "Untested Inspiring Admiral",
         "Experienced Inspiring Admiral",
         "Seasoned Inspiring Admiral"
-      ]
+      ],
+      initiativeCards: [
+        { 
+        name: "Zeal",
+        initiativeValue: 1
+      },
+      { 
+        name: "Long Range Gunnery",
+        initiativeValue: 2
+      },
+      { 
+        name: "Adjust Formation",
+        initiativeValue: 3
+      },
+      { 
+        name: "Enagage More Closely",
+        initiativeValue: 4
+      },
+      { 
+        name: "Fast Loader",
+        initiativeValue: 5
+      }
+    ]  
     },
     {
-      value: 'French',
+      name: 'French',
       admirals: [
         "Untested Admiral",
         "Experienced Admiral",
@@ -143,11 +194,32 @@ function App() {
         "Untested Inspiring Admiral",
         "Experienced Inspiring Admiral",
         "Seasoned Inspiring Admiral"
-
-      ]
+      ],
+      initiativeCards: [
+        { 
+        name: "Elan",
+        initiativeValue: 1
+      },
+      { 
+        name: "Superior Firepower",
+        initiativeValue: 2
+      },
+      { 
+        name: "Target Rigging",
+        initiativeValue: 3
+      },
+      { 
+        name: "Swift Vessels",
+        initiativeValue: 4
+      },
+      { 
+        name: "Boarders Away",
+        initiativeValue: 5
+      }
+    ]  
     },
     {
-      value: 'Spanish',
+      name: 'Spanish',
       admirals: [
         "Untested Admiral",
         "Experienced Admiral",
@@ -173,10 +245,32 @@ function App() {
         "Untested Inspiring Admiral",
         "Experienced Inspiring Admiral",
         "Seasoned Inspiring Admiral"
-      ]
+      ],
+      initiativeCards: [
+        { 
+        name: "Repel Boarders",
+        initiativeValue: 1
+      },
+      { 
+        name: "Heavy Musket Volley",
+        initiativeValue: 2
+      },
+      { 
+        name: "Aggression",
+        initiativeValue: 3
+      },
+      { 
+        name: "Resilient",
+        initiativeValue: 4
+      },
+      { 
+        name: "Bravado",
+        initiativeValue: 5
+      }
+    ] 
     },
     {
-      value: 'Pirate',
+      name: 'Pirate',
       admirals: [
         "Untested Admiral",
         "Experienced Admiral",
@@ -203,7 +297,17 @@ function App() {
         "Untested Inspiring Admiral",
         "Experienced Inspiring Admiral",
         "Seasoned Inspiring Admiral",
-      ]
+      ],
+      initiativeCards: [
+        { 
+        name: "Deception",
+        initiativeValue: 2
+      },
+      { 
+        name: "Raise the Black",
+        initiativeValue: 3
+      }
+    ] 
     }
   ];
 
@@ -230,6 +334,77 @@ function App() {
     },
   ];
 
+  const genericInitiativeCards = [
+    {
+      name: "Await New Orders",
+      initiativeValue: 1
+    },
+    {
+      name: "Down!",
+      initiativeValue: 1
+    },
+    {
+      name: "Fire as She Bears",
+      initiativeValue: 1
+    },
+    {
+      name: "Intrepid (special rule)",
+      initiativeValue: 1
+    },
+    {
+      name: "Careful Aim",
+      initiativeValue: 2
+    },
+    {
+      name: "Take Courage",
+      initiativeValue: 2
+    },
+    {
+      name: "Effective Fire",
+      initiativeValue: 3
+    },
+    {
+      name: "Small Arms",
+      initiativeValue: 3
+    },
+    {
+      name: "Reload!",
+      initiativeValue: 3
+    },
+    {
+      name: "Rolling Broadsides",
+      initiativeValue: 3
+    },
+    {
+      name: "Adjust Position",
+      initiativeValue: 4
+    },
+    {
+      name: "Fleet Maneuver",
+      initiativeValue: 4
+    },
+    {
+      name: "Lee Guage Fire",
+      initiativeValue: 4
+    },
+    {
+      name: "Shoals Ahead",
+      initiativeValue: 4
+    },
+    {
+      name: "Boarding Through the Smoke",
+      initiativeValue: 4
+    },
+    {
+      name: "Doughty (Special rule)",
+      initiativeValue: 4
+    },
+    {
+      name: "Favorable Wind",
+      initiativeValue: 5
+    }
+  ];
+
   return (
     <div className="App">
 
@@ -250,7 +425,25 @@ function App() {
       </FormControl>
 
       <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-label">Choose Faction</InputLabel>
+        <InputLabel>Choose Faction</InputLabel>
+        <Select
+          displayEmpty
+          className={classes.selectEmpty}
+          label="Select faction"
+          value={faction.name}
+          onChange={handleFactionChange}
+          helperText="Faction">
+          {factions.map((option) => (
+            <MenuItem key={option.name} value={option.name}>
+              {option.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+
+      {faction.name === "Pirate" ?(
+      <FormControl className={classes.formControl}>
+        <InputLabel>Choose Pirate Faction</InputLabel>
         <Select
           displayEmpty
           className={classes.selectEmpty}
@@ -258,22 +451,22 @@ function App() {
           value={faction.value}
           onChange={handleFactionChange}
           helperText="Faction">
-          {factions.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.value}
+          {factions.filter(x => x.name !== "Pirate").map((option) => (
+            <MenuItem key={option.name} value={option.name}>
+              {option.name}
             </MenuItem>
           ))}
         </Select>
       </FormControl>
-
+      ) : null}
 
       <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-label">Choose Admiral</InputLabel>
+        <InputLabel>Choose Admiral</InputLabel>
         <Select
           displayEmpty
           className={classes.selectEmpty}
           label="Select Admiral"
-          disabled={faction == ""}
+          disabled={faction === ""}
           value={admiral}
           onChange={handleAdmiralChange}
           helperText="Admiral">
@@ -291,7 +484,16 @@ function App() {
         className={classes.button}
         variant="contained"
         onClick={handleAddShip}>
-          Add Ship
+        Add Initiative card
+      </Button>
+
+      <Divider />
+
+      <Button
+        className={classes.button}
+        variant="contained"
+        onClick={handleAddShip}>
+        Add Ship
       </Button>
 
     </div>
