21a3c6cef8ddc20a18ba75a9c749663a9f67bd78
Djaa9 <jp@thepage.dk>
2020-07-18 14:16:11 +0200

    added ships

--- src/data/ships.js
+++ src/data/ships.js
@@ -1,3 +1,138 @@
   export const Ships = [
-
+    {
+      name: "Sloop",
+      upgrades: [
+        "Additional Crew", 
+        "Additional Guns",
+        "Sweeps",
+        "Swift"
+      ],
+      factions: ["English", "Spanish", "Dutch", "French", "Pirate"]
+    },
+    {
+      name: "Corvette",
+      upgrades: [
+        "Additional Crew", 
+        "Additional Guns",
+        "Sweeps",
+        "Swift"
+      ],
+      factions: ["English", "Spanish", "Dutch", "French", "Pirate"]
+    },
+    {
+      name: "Brigantine",
+      upgrades: [
+        "Additional Crew", 
+        "Additional Guns",
+        "Sweeps",
+        "Swift",
+        "Weatherly"
+      ],
+      factions: ["English", "Spanish", "Dutch", "French", "Pirate"]
+    },
+    {
+      name: "Fluyt",
+      upgrades: [
+        "Additional Crew", 
+        "Additional Guns",
+        "Stout"
+      ],
+      factions: ["English", "Spanish", "Dutch", "French", "Pirate"]
+    },
+    {
+      name: "Petite Fregate",
+      upgrades: [
+        "Additional Crew", 
+        "Additional Guns",
+        "Sweeps",
+        "Weatherly"
+      ],
+      factions: ["English", "Spanish", "Dutch", "French", "Pirate"]
+    },
+    {
+      name: "Light Galleon",
+      upgrades: [
+        "Additional Crew", 
+        "Additional Guns",
+        "Stout",
+        "Weatherly"
+      ],
+      factions: ["English", "Spanish", "Dutch", "French", "Pirate"]
+    },
+    {
+      name: "Sixth Rate",
+      upgrades: [
+        "Additional Crew", 
+        "Additional Guns",
+        "Stout",
+        "Swift",
+        "Weatherly"
+      ],
+      factions: ["English", "Spanish", "Dutch", "French", "Pirate"]
+    },
+    {
+      name: "Fifth Rate",
+      upgrades: [
+        "Additional Crew", 
+        "Additional Guns",
+        "Stout",
+        "Swift",
+        "Weatherly"
+      ],
+      factions: ["English", "Spanish", "Dutch", "French", "Pirate"]
+    },
+    {
+      name: "Fourth Rate",
+      upgrades: [
+        "Additional Crew", 
+        "Additional Guns",
+        "Stout",
+        "Swift",
+        "Weatherly"
+      ],
+      factions: ["English", "Spanish", "Dutch", "French", "Pirate"]
+    },
+    {
+      name: "Third Rate",
+      upgrades: [
+        "Additional Crew", 
+        "Additional Guns",
+        "Stout",
+        "Swift"
+      ],
+      factions: ["English", "Spanish", "Dutch", "French", "Pirate"]
+    },
+    {
+      name: "Second Rate",
+      upgrades: [
+        "Additional Crew", 
+        "Additional Guns",
+        "Stout",
+        "Swift",
+        "Weatherly"
+      ],
+      factions: ["English", "Spanish", "Dutch", "French", "Pirate"]
+    },
+    ,
+    {
+      name: "First Rate",
+      upgrades: [
+        "Additional Crew", 
+        "Additional Guns",
+        "Stout",
+        "Swift",
+        "Weatherly"
+      ],
+      factions: ["English", "Spanish", "Dutch", "French", "Pirate"]
+    },			
+    {
+      name: "Revenge",
+      upgrades: [],
+      factions: ["Pirate"]
+    },
+    {
+      name: "Queen Anne's Revenge",
+      upgrades: [],
+      factions: ["Pirate"]
+    }
   ];
