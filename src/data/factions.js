6fbf43d904f38548310be1c189cc4b33718a86ec
Djaa9 <jp@thepage.dk>
2020-08-06 21:42:47 +0200

    added paths for card images to faction

--- src/data/factions.js
+++ src/data/factions.js
@@ -3,22 +3,27 @@ import { factionTypes } from "./factionTypes";
 export const factions = [
     {
       name: 'Dutch',
-      type: factionTypes.DUTCH
+      type: factionTypes.DUTCH,
+      image: "http://oai.thepage.dk/cardImages/factions/dutch.png"
     },
     {
       name: 'English',
-      type: factionTypes.ENGLISH
+      type: factionTypes.ENGLISH,
+      image: "http://oai.thepage.dk/cardImages/factions/english.png"
     },
     {
       name: 'French',
-      type: factionTypes.FRENCH
+      type: factionTypes.FRENCH,
+      image: "http://oai.thepage.dk/cardImages/factions/french.png"
     },
     {
       name: 'Spanish',
-      type: factionTypes.SPANISH
+      type: factionTypes.SPANISH,
+      image: "http://oai.thepage.dk/cardImages/factions/spanish.png"
     },
     {
       name: 'Pirate',
-      type: factionTypes.PIRATE
+      type: factionTypes.PIRATE,
+      image: "http://oai.thepage.dk/cardImages/factions/pirates.png"
     }
   ];
